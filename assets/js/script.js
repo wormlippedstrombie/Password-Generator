0ecca239f675205546614efa55126bb95b88cf89
wormlippedstrombie <tysonleethornton@outlook.com>
2023-07-05 08:48:46 +0930

    added a basic password generation code that now needs to have features for variable password length and special character ommision added to the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+function generatePassword () {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 15;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    return password;
+  }
+  
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
