d87d27bbc83bfd1b23125c474c5487e4087db52f
wormlippedstrombie <tysonleethornton@outlook.com>
2023-07-05 09:18:25 +0930

    have added prompts that ask the user to select password criteria including password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,35 @@ function writePassword() {
   passwordText.value = password;
 }
 function generatePassword () {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 15;
+    var numberChars = "0123456789";
+    var specialChars = "!@#$%^&*()";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+
+    var passwordLength;
+    do {
+        passwordLength = parseInt(prompt("Enter password length (8-128 characters):"));
+    } while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
     var password = "";
-    for (var i = 0; i <= passwordLength; i++) {
+
+    var includeSpecialChars = confirm("Include special characters?");
+    var includeNumbers = confirm("Include numbers?");
+    var includeUppercase = confirm("Include uppercase letters?");
+
+    var chars = lowercaseChars;
+    if(includeSpecialChars) {
+        chars += specialChars;
+    }
+    if (includeNumbers) {
+        chars += numberChars;
+    }
+    if (includeUppercase) {
+        chars += uppercaseChars
+    }
+
+    for (var i = 0; i < passwordLength; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber +1);
+        password += chars.substring(randomNumber, randomNumber +1 );
     }
     return password;
   }
